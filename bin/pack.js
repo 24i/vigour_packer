855b03911276e653b7857ae637e29847cf1d2814
Renan Carvalho <renansscarvalho@gmail.com>
2016-04-13 17:41:55 +0200

    started to handle commandline args

--- bin/pack.js
+++ bin/pack.js
@@ -3,7 +3,18 @@
 
 let Config = require('vigour-config')
 let Pack = require('../lib')
+let argumentData = []
+// 1st = Platform
+// 2nd = EntryPoint folder
+// 3rd = Output folder
+process.argv.forEach(function (val, index, array) {
+	if(index > 2) {
+		argumentData.push(val)
+	}
+})
 
 let config = new Config()
 let pack = Pack()
-pack.run()
+
+console.log(argumentData)
+pack.run(argumentData)
